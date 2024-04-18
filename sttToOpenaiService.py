11c8518a71d4d32526a895334b69122cfbb771d0
chaewu <dev.chaewu@gmail.com>
2024-04-18 11:26:19 +0900

    REFACTOR : 클래스 단위 리팩토링

--- sttToOpenaiService.py
+++ sttToOpenaiService.py
@@ -8,21 +8,35 @@ import pyaudio
 import wave
 from openai import OpenAI
 
+dotenv.load_dotenv('.env')
+
 
 def delay():
-    time.sleep(5)
+    time.sleep(3)
 
 
-class STTService:
+class SttService:
     def __init__(self):
-        self.input_source = sd.query_devices()  # 음성 데이터 입력 input 확인
-        dotenv.load_dotenv('.env')
+        self.input_source = sd.query_devices()
         self.FORMAT = pyaudio.paInt16
         self.CHANNELS = 1
         self.RATE = 48000
         self.CHUNK = 1024
         self.RECORD_SECONDS = 5
         self.audio = pyaudio.PyAudio()
+        self.config = {}
+        self.files = {"file": open("audio_files/audio.wav", "rb")}
+        self.getMediaIdData = {"config": json.dumps(self.config)}
+        self.headers = {"Authorization": "bearer " + self.get_token()}
+        self.getTokenData = {
+            "client_id": os.getenv('CLIENT_ID'),
+            "client_secret": os.getenv('CLIENT_SECRET')
+        }
+        self.urls = {
+            "getTokenUrl": "https://openapi.vito.ai/v1/authenticate",
+            "getMediaIdUrl": "https://openapi.vito.ai/v1/transcribe",
+            "speachToTextUrl": "https://openapi.vito.ai/v1/transcribe" + self.get_media_id()
+        }
 
     def save_wav_file(self):
         stream = self.audio.open(format=self.FORMAT,
@@ -50,7 +64,7 @@ class STTService:
         self.audio.terminate()
 
         # 녹음된 데이터를 WAV 파일로 저장
-        wavefile = wave.open("audio_files/depress.wav", 'wb')
+        wavefile = wave.open("audio_files/audio.wav", 'wb')
         wavefile.setnchannels(self.CHANNELS)
         wavefile.setsampwidth(self.audio.get_sample_size(self.FORMAT))
         wavefile.setframerate(self.RATE)
@@ -62,36 +76,27 @@ class STTService:
         delay()
 
     def get_token(self):
-        resp = requests.post(
-            'https://openapi.vito.ai/v1/authenticate',
-            data={'client_id': os.getenv('CLIENT_ID'),
-                  'client_secret': os.getenv('CLIENT_SECRET')}
-        )
+        resp = requests.post(self.urls["getTokenUrl"], data=self.getTokenData)
         resp.raise_for_status()
         token = resp.json().get('access_token')
+
         delay()
 
         return token
 
-    def get_media_id(self, token):
-        config = {}
-        resp = requests.post(
-            'https://openapi.vito.ai/v1/transcribe',
-            headers={'Authorization': 'bearer ' + token},
-            data={'config': json.dumps(config)},
-            files={'file': open('audio_files/depress.wav', 'rb')}
-        )
+    def get_media_id(self):
+        resp = requests.post(self.urls["getMediaIdUrl"], headers=self.headers, data=self.getMediaIdData,
+                             files=self.files)
+
         resp.raise_for_status()
         media_id = resp.json().get('id')
+
         delay()
 
         return media_id
 
-    def stt(self, token, media_id):
-        resp = requests.get(
-            'https://openapi.vito.ai/v1/transcribe/' + media_id,
-            headers={'Authorization': 'bearer ' + token},
-        )
+    def speach_to_text(self):
+        resp = requests.get(self.urls["speachToTextUrl"], headers=self.headers)
         resp.raise_for_status()
         speach_to_text = resp.json()['results']['utterances'][0]['msg']  # json return data 중 msg string 부분 출력
         delay()
@@ -100,33 +105,35 @@ class STTService:
 
 
 class OpenAIService:
-    def __init__(self):
-        dotenv.load_dotenv('.env')
+    def __init__(self, text):
+        self.model = "gpt-3.5-turbo"
+        self.client = OpenAI(api_key=os.getenv('OPEN_API_KEY'))
 
-    def openai_parser(self, text):
-        client = OpenAI(api_key=os.getenv('OPEN_API_KEY'))
+        self.messages = [
+            {
+                "role": "system",
+                "content": "From now on, you are an AI model that recommends teas based on the user’s mood."
+            },
 
-        model = "gpt-3.5-turbo"
-        query = text
-        messages = [{"role": "system",
-                     "content": "From now on, you are an AI model that recommends teas based on the user’s mood."},
-                    {"role": "user",
-                     "content": query}]
+            {
+                "role": "user", "content": text
+            }
+        ]
 
-        completion = client.chat.completions.create(model=model, messages=messages)
+    def openai_parser(self):
+        completion = self.client.chat.completions.create(model=self.model, messages=self.messages)
         open_ai_text = completion.choices[0].message.content
+
         delay()
 
         return open_ai_text
 
 
 if __name__ == '__main__':
-    stt = STTService()
-    openai = OpenAIService()
+    A = SttService()
+    A.save_wav_file()
+    userSpeach = A.speach_to_text()
 
-    # stt.save_wav_file()
-    api_token = stt.get_token()
-    result = stt.stt(api_token, stt.get_media_id(api_token))
+    print("User : " + userSpeach)
 
-    print("User : " + result)
-    print("Tea Bot : " + openai.openai_parser(result))
+    print("TeaBot : " + OpenAIService(userSpeach).openai_parser())
